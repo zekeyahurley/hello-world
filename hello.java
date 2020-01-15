b37d5714d902764ddfd1d3bc07296c529da0624c
zekeyahurley <59926308+zekeyahurley@users.noreply.github.com>
2020-01-15 10:24:02 -0500

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,18 @@
+/**********************************
+* Hello.java
+***********************************
+* Zekeya Hurley
+* Jan 15 2020
+* CMCS 255 003
+**********************************/
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("<Zekeya Hurley>");
+		System.out.println("<Hello.java>");
+		System.out.println("<Lab 1 Java Setup>");
+		System.out.println("<Java version 1.8.0_242>");
+		System.out.println("<CMCS 255 003>");
+		System.out.println();
+	}
+}
